29164c9e8c48743745b732880d55c40d832cce00
Arul Prasad <arulprasad@gmail.com>
2013-03-28 19:44:09 +0800

    pick image, read exif and show it in textfield

--- ExifReader/ERViewController.h
+++ ExifReader/ERViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ERViewController : UIViewController
+@interface ERViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
 
 @end
--- ExifReader/ERViewController.m
+++ ExifReader/ERViewController.m
@@ -8,8 +8,16 @@
 
 #import "ERViewController.h"
 
-@interface ERViewController ()
+#import <AssetsLibrary/AssetsLibrary.h>
 
+
+@interface ERViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIButton *pickButton;
+@property (nonatomic) UIImagePickerController *picker;
+
+@property (nonatomic) BOOL firstTimeView;
 @end
 
 @implementation ERViewController
@@ -17,7 +25,51 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[self pickButton] addTarget:self action:@selector(onPickButtonTouchDown:) forControlEvents:UIControlEventTouchDown];
+    self.picker = [[UIImagePickerController alloc]init];
+
+
+}
+
+-(void) viewDidAppear:(BOOL)animated{
+    if(!self.firstTimeView){
+    [self showImagePicker];
+    }
+    self.firstTimeView = true;
+}
+
+-(void)onPickButtonTouchDown:(id) sender{
+    [self showImagePicker];
+}
+
+-(void)showImagePicker{
+    
+    self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    self.picker.delegate = self;
+    [self presentViewController: self.picker animated:YES completion:nil];
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
+    
+    
+    NSURL *assetURL = [info objectForKey:UIImagePickerControllerReferenceURL];
+    
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    __block NSMutableDictionary *imageMetadata = nil;
+    [library assetForURL:assetURL
+             resultBlock:^(ALAsset *asset)  {
+                 NSDictionary *metadata = asset.defaultRepresentation.metadata;
+                 imageMetadata = [[NSMutableDictionary alloc] initWithDictionary:metadata];
+                 self.textView.text = imageMetadata.description;
+                 [self.textView selectAll:self];
+                 [self.imageView setImage:[info objectForKey:UIImagePickerControllerOriginalImage]];
+             }
+            failureBlock:^(NSError *error) {
+            }];
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
